10f2ef5b2ea9ef4348904c4490b1f224cc6b1220
Laania11 <alopezl90@miumg.edu.gt>
2026-05-14 22:47:05 -0600

    config: add postgres profile and sql scripts

---
+++ sql/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO nodes (value, parent_id) VALUES ('Gerencia', NULL);
+INSERT INTO nodes (value, parent_id) VALUES ('Jefatura TI', 1);
+INSERT INTO nodes (value, parent_id) VALUES ('Desarrollador', 2);
